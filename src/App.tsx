c2db88f78d575b32e195afb0b1b0b4b81ea42309
NaziaHladun <fabulous7351@gmail.com>
2025-02-26 16:28:44 +0200

    created a Header and added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
-  return <>Page</>;
-}
+import RootLayout from "./pages/RootLayout";
+import HomePage from "./pages/HomePage";
+import RecipePage from "./pages/RecipePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/recipe/:id", element: <RecipePage /> },
+    ],
+  },
+]);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import { TextField, Box, styled } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchRecipes } from "../store/features/recipeSlice";
+import { AppDispatch } from "../store/store";
+
+export const Header = () => {
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      dispatch(fetchRecipes(inputValue));
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [inputValue, dispatch]);
+
+  return (
+    <HeaderContainer>
+      <TextField
+        label="Search recipe"
+        size="small"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled(Box)(() => ({
+  backgroundColor: "#E4EFE7",
+  height: "80px",
+  gap: 2,
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+  position: "relative",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+}));
